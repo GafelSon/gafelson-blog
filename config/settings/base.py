02f91296346f311ecb2eb23f8a1792c0dff64985
gafelson <gafelson.soheil@gmail.com>
2025-02-04 14:39:31 +0330

    chore: add base.py for common settings configuration

---
+++ config/settings/base.py
@@ -0,0 +1,117 @@
+from pathlib import Path
+from django.core.management.utils import get_random_secret_key
+import os
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+# Security - Will be overridden in production
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
+ADMIN_URL = os.getenv("ADMIN_URL", "admin")
+
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "compressor",
+    "ckeditor",
+    "imagekit",
+    "lazy_srcset",
+    "blog.apps.BlogConfig",
+]
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django.middleware.gzip.GZipMiddleware",
+    "django.middleware.security.SecurityMiddleware",
+]
+
+ROOT_URLCONF = "config.urls"
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "templates"],
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+            "loaders": [
+                (
+                    "django.template.loaders.cached.Loader",
+                    [
+                        "django.template.loaders.filesystem.Loader",
+                        "django.template.loaders.app_directories.Loader",
+                    ],
+                ),
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = "config.wsgi.application"
+
+# Password validation
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
+
+# Internationalization
+LANGUAGE_CODE = "fa-IR"
+TIME_ZONE = "Asia/Tehran"
+USE_I18N = True
+USE_TZ = True
+
+# Static files
+STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / "blog/templates/public"]
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+    "compressor.finders.CompressorFinder",
+]
+
+# Django Compressor
+COMPRESS_ENABLED = True
+
+# Default primary key field
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Silence specific warnings
+SILENCED_SYSTEM_CHECKS = [
+    "ckeditor.W001",
+]
+
+# SEO Defaults
+SITE_NAME = "Gaf Blog"
+DEFAULT_OG_IMAGE_URL = "/static/images/default-og.jpg"
+LOGO_URL = "/static/images/logo.jpg"

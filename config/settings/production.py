db738b7c33806a76f9acb7fd7e82ba10e63b067b
gafelson <gafelson.soheil@gmail.com>
2025-02-08 16:41:38 +0330

    fix #1009

--- config/settings/production.py
+++ config/settings/production.py
@@ -2,21 +2,21 @@ from .base import *
 import os
 
 # Security
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "localhost,127.0.0.1,[::1]").split(",")
 
 # Caching (Use Redis/Memcached)
-CACHES = {
-    "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-            "COMPRESSOR": "django_redis.compressors.zlib.ZlibCompressor",
-        },
-        "KEY_PREFIX": "blog",
-    }
-}
+# CACHES = {
+#    "default": {
+#        "BACKEND": "django_redis.cache.RedisCache",
+#        "LOCATION": "redis://127.0.0.1:6379/1",
+#        "OPTIONS": {
+#            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+#            "COMPRESSOR": "django_redis.compressors.zlib.ZlibCompressor",
+#        },
+#        "KEY_PREFIX": "blog",
+#    }
+#}
 
 # Database Optimization (PostgreSQL recommended)
 DATABASES = {
